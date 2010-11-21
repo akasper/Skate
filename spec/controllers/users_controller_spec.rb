f4f9468bbad5a98dcfc4e1641c9a80780864c137
Andrew Kasper <kasper@vitrue.com>
2010-11-21 18:22:21 -0500

    Basic user functionality.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'spec_helper'
 
+def invalid_email_expectations params={}
+  before :each do
+    post 'create', :user => params
+  end
+  
+  it 'redirects to the "new user" page' do
+    response.should redirect_to new_user_path
+  end
+  it 'provides meaningful feedback' do
+    flash[:error].should_not be_blank
+  end
+end
+
 describe UsersController do
+  SAMPLE_USER_DATA = {:email => 'foo@example.com'} unless defined? SAMPLE_USER_DATA
+
   context :new do
     it 'is an action' do
       controller.should be_respond_to(:new)
@@ -14,7 +29,7 @@ describe UsersController do
     
     it 'should render the new template' do
       get 'new'
-      response.should render_template('users/new')
+      response.should render_template 'users/new'
     end
   end
   
@@ -32,17 +47,35 @@ describe UsersController do
       it 'creates a user object' do
         User.all.count.should == 1
       end
-      it 'redirects to the "show user" page'
+      it 'redirects to the "show user" page' do
+        response.should redirect_to (user_path User.first)
+      end
     end
     
     context 'receives invalid user data' do
-      it 'redirects to the "new user" page'
-      ['an invalid email adddress', 'no email address'].each do |error|
-        describe "such as #{error}" do
-          it 'provides meaningful feedback'
+      
+      describe 'such as no "user" field in the params hash' do
+        it 'redirects to the new user page' do
+          post :create, {}
+          response.should redirect_to new_user_path
         end
       end
+      
+      describe 'such as an invalid email address' do
+        invalid_email_expectations :email => 'bademailaddress'
+      end
+      describe 'such as no email address' do
+        invalid_email_expectations :email => ''
+        invalid_email_expectations :email => nil
+      end
+      describe 'such as the submitted email address already existing' do
+        before :each do
+          post :create, :user => SAMPLE_USER_DATA
+          User.all.count.should == 1
+        end
+        invalid_email_expectations SAMPLE_USER_DATA
+      end
     end
-  end
-  
+  end  
 end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ describe User do
   it 'has an email' do
     User.new.should be_respond_to(:email)
   end
+  
+  it 'has a valid email' do
+    @user = User.new(:email => 'bademailaddress')
+    @user.save.should == false
+  end
 end
